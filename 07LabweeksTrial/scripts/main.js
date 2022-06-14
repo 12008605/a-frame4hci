9d5781f6c123f98194841e59a69923ab8f52d1e8
12008605 <107105434+12008605@users.noreply.github.com>
2022-06-14 11:23:10 +0200

    Update main.js

--- 07LabweeksTrial/scripts/main.js
+++ 07LabweeksTrial/scripts/main.js
@@ -1,26 +1,3 @@
-
-var rotationSpeed = 0.001;
-var myOtherBox = document.getElementById('myOtherBox');
-
-function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
-	myOtherBox.object3D.rotation.y += rotationSpeed;
-	myOtherBox.object3D.rotation.z += rotationSpeed/2;
-	//console.log(myOtherBox.object3D.rotation);
-}
-setInterval(spin, 10);
-
-
-myOtherBox.addEventListener('mouseenter', function(){
-	rotationSpeed = 0.01;
-	console.log('entered');
-});
-
-myOtherBox.addEventListener('mouseleave', function(){
-	rotationSpeed = 0.001;
-	console.log('left');
-});
-
 let nextLevel = 'index.html';
 
 const shoot = () => {
